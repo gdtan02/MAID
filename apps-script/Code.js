0aef25f98578f6c76defbc345027f57ad93660f6
tgd1115 <gdtan021115@gmail.com>
2024-07-24 20:11:03 +0800

    Initial commit - Google Apps Script setup

---
+++ apps-script/Code.js
@@ -0,0 +1,13 @@
+function test() {
+  // Test purpose only
+  
+}
+
+function test2() {
+    // Test local push
+}
+
+function testLocal() {
+    // Test local push versioning
+    // Main branch
+}
